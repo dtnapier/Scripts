944886c1604b304cd7aacc1d95f56d6567da1975
Tyler Napier <dtnapier@hotmail.com>
2020-05-26 15:33:21 -0500

    Delete Check Locked Users.ps1

--- Check Locked Users.ps1
+++
@@ -1,9 +0,0 @@
-﻿$properties = @(
-    @{n='User';e={$_.Properties[0].Value}},
-    @{n='Locked by';e={$_.Properties[1].Value}},
-    @{n='TimeStamp';e={$_.TimeCreated}}
-    @{n='DCname';e={$_.Properties[4].Value}}
-)
-Get-WinEvent -ComputerName TRPCTS1 -FilterHashTable @{LogName='Security'; ID=4740} | 
-Select $properties |
-Export-csv C:\Users\$env:username\Desktop\LockedUsers.csv -NoTypeInformation -Encoding UTF8
