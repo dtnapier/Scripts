5e1a9fc1ccddb34bd19bff16fb24e0b5a1d29c1c
Tyler Napier <dtnapier@hotmail.com>
2020-06-01 09:23:19 -0500

    Rename Search_Command.ps1 to StringSearch.ps1

--- Powershell/Search_Command.ps1
+++
@@ -1,4 +0,0 @@
-#use this command to search for accounts in this folder
-#replace "username" with whatever you want to search for
-
-Get-ChildItem -Path '\\trpcweb.com\sdata\Technology\InfoTechnology\Procedures\User Accounts\Domain' -recurse | Select-String -pattern "username" 
